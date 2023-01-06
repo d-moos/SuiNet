5e27d7aac1d0e79540f8c9f02bc6306d593acdd9
Dani <dani@localhost>
2023-01-06 10:52:23 +0100

    remove github nuget source, add cursor-based streaming for events

--- Naami.SuiNet.Extensions/ApiStreams/EventApiExtensions.cs
+++ Naami.SuiNet.Extensions/ApiStreams/EventApiExtensions.cs
@@ -38,4 +38,39 @@ public static class EventApiExtensions
             yield return page.Data;
         }
     }
+    
+    public static async IAsyncEnumerable<SuiEventEnvelope[]> GetEventStream(
+        this IEventApi eventApi,
+        EventId initialCursor,
+        int pageSize = 100,
+        bool isDescending = false
+    )
+    {
+        var page = await eventApi.GetEvents((uint)pageSize, isDescending);
+        yield return page.Data;
+
+        while (page.NextCursor != null)
+        {
+            page = await eventApi.GetEvents(page.NextCursor, (uint)pageSize, isDescending);
+            yield return page.Data;
+        }
+    }
+    
+    public static async IAsyncEnumerable<SuiEventEnvelope[]> GetEventStream<TEventFilter>(
+        this IEventApi eventApi,
+        TEventFilter query,
+        EventId initialCursor,
+        int pageSize = 100,
+        bool isDescending = false
+    ) where TEventFilter : IEventFilter
+    {
+        var page = await eventApi.GetEvents(query, initialCursor, (uint)pageSize, isDescending);
+        yield return page.Data;
+
+        while (page.NextCursor != null)
+        {
+            page = await eventApi.GetEvents(query, page.NextCursor, (uint)pageSize, isDescending);
+            yield return page.Data;
+        }
+    }
 }
