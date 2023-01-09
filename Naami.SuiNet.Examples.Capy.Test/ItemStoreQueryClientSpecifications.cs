5e3802d2209c7f667e2ed9ed4cbf097090758723
Dani <dani@localhost>
2023-01-09 14:44:29 +0100

    unversioned files oops

---
+++ Naami.SuiNet.Examples.Capy.Test/ItemStoreQueryClientSpecifications.cs
@@ -0,0 +1,26 @@
+using Naami.SuiNet.JsonRpc;
+
+namespace Naami.SuiNet.Examples.Capy.Test;
+
+public class Tests
+{
+    private readonly ItemStoreQueryClient _itemStoreQueryClient = new(
+        new JsonRpcClient("https://fullnode.devnet.sui.io"),
+        "0x30136e80e16ac92ff33a0ac2d67c64dc129eb0bc",
+        "0x3c4706fd9deec1674137d92c1a2296001489fa46"
+    );
+
+    [Test]
+    public async Task ItemStore()
+    {
+        var store = await _itemStoreQueryClient.GetStore();
+        Assert.That(!string.IsNullOrEmpty(store.Balance));
+    }
+    
+    [Test]
+    public async Task ItemStore_Items()
+    {
+        var store = await _itemStoreQueryClient.GetAvailableItems();
+        Assert.That(store.Length > 0);
+    }
+}
